b9d3a48df22348e358ac3cb3a6086ba26824329b
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-05-20 01:03:16 -0400

    Initial setup

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <UApp>
+    <NuxtPage />
+    <UButton>test</UButton>
+  </UApp>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  vite: {
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit"],
+    },
+  },
+});
