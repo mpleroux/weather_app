163e5cabaa4085b88e011c3551f8353077d9b5b3
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-06-05 14:44:37 -0400

    Prevent icon from shrinking in narrow viewports

--- app/components/MeteoIcon.vue
+++ app/components/MeteoIcon.vue
@@ -9,5 +9,5 @@ const props = defineProps<{
   <div
     :style="{ '--mask-url': `url('/meteocons/monochrome/${props.name}.svg')` }"
     :class="size"
-    class="mask-size-contain mask-position-center bg-slate-700 mask-(--mask-url) mask-alpha mask-no-repeat dark:bg-slate-300" />
+    class="mask-size-contain mask-position-center shrink-0 bg-slate-700 mask-(--mask-url) mask-alpha mask-no-repeat dark:bg-slate-300" />
 </template>
